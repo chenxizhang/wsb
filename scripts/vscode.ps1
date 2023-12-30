247a6f46987564cde16928ded6859ff702f79a35
chenxizhang <ares@xizhang.com>
2023-12-30 16:14:05 +0800

    update

--- scripts/vscode.ps1
+++ scripts/vscode.ps1
@@ -2,10 +2,10 @@ Set-ExecutionPolicy Unrestricted -Force
 
 # curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Downloads\vscode.exe
 
-C:\users\WDAGUtilityAccount\Downloads\sandbox\vscode.exe /verysilent /suppressmsgboxes
+winget install vscode -h
 
 code --install-extension ms-ceintl.vscode-language-pack-zh-hans
 
-@{ "locale"="zh-cn"} | ConvertTo-Json |  Out-File -Encoding utf8 -FilePath $home\.vscode\argv.json
+@{ "locale" = "zh-cn" } | ConvertTo-Json |  Out-File -Encoding utf8 -FilePath $home\.vscode\argv.json
 
 
